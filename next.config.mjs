994363b90066d9399a567b47a4111e6f93a3f88e
максим <maksimkrygliak@gmail.com>
2024-10-15 16:57:20 +0300

    Обновление данных

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  i18n: {
+    locales: ['uk', 'en'], // Поддерживаемые языки: украинский и английский
+    defaultLocale: 'uk',   // Язык по умолчанию - украинский
+  },
 };
   
-  export default nextConfig;
+export default nextConfig;
